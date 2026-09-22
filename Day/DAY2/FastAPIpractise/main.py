14c527d284d81d37090faeaccf56c7ac5453199e
chaitanya wazir <2025cs_chaitanyawazir_b@nie.ac.in>
2026-09-22 12:53:44 +0530

    adding ultiple routes

--- Day/DAY2/FastAPIpractise/main.py
+++ Day/DAY2/FastAPIpractise/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
-def read_root():
-    return {"message": "hello world","number": 123,"is fun":True}    
+def home():
+    return {"page": "Home"}    
+
+@app.get("/about")
+def about():
+    return {"page": "About","author": "Chakshu"}
+@app.get("/health")
+def contact():
+    return {"status": "ok"}
